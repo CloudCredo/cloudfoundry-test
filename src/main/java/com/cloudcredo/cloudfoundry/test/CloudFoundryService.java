e1f2bdb6849c82a7121e9c22aab66d1607dd5272
Christopher Hedley <chris@cghsystems.net>
2013-05-18 15:19:32 +0100

    More loggig, disabled MongoDB test for now

--- src/main/java/com/cloudcredo/cloudfoundry/test/CloudFoundryService.java
+++ src/main/java/com/cloudcredo/cloudfoundry/test/CloudFoundryService.java
@@ -2,9 +2,9 @@ package com.cloudcredo.cloudfoundry.test;
 
 public enum CloudFoundryService {
 
-    RABBITMQ("rabbitmq", "rabbit_node_free_0", "RMQaaS.provision.rabbit_node_free", "2.4"),
-    REDIS("redis", "redis_node_0", "RaaS.provision.redis_node_0", "1.8"),
-    MONGODB("mongodb", "mongodb_node_0", "RaaS.provision.mongodb_node_0", "2.8");
+    RABBITMQ("rabbitmq", "rabbit_node", "RMQaaS.provision.rabbit_node_free", "2.4"),
+    REDIS("redis", "redis_node", "RaaS.provision.redis_node_0", "1.8"),
+    MONGODB("mongodb", "mongodb_node", "RaaS.provision.mongodb_node_0", "2.8");
 
     public final String serviceName;
     public final String serviceNode;
